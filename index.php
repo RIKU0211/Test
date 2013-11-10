8c6aa69de80cf3d48e71ec2435845ac44b86c7f8
RIKU0211 <x.blade.riku@gmail.com>
2013-11-10 04:55:16 -0800

    indedx.php update

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+session_start();
+
+?>
+
+<html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>sample_shop</title>
+    </head>
+    <body background="image/bg.jpg">
+
+
+    <h2>[KINGDOM HEARTS-SHOP]</h2>
+
+    <br><br>
+    welcome!
+    <hr>
+    <br>
+
+    <!-- user -->
+    <a href="input.html">ユーザ登録</a>
+    <br><br>
+
+
+    <br><br>
+
+    <!-- 商品ページへ -->
+    <a href="menu.php">商品一覧</a>
+    <br><br>
+
+    <hr>
+
+    </body>
+    </html>
